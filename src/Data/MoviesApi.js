ebab69596e5728957c6524177952444daaee28e4
RamezMathlouthi <ramezmathlouthi444@gmail.com>
2024-02-01 21:36:52 +0100

    first commit

---
+++ src/Data/MoviesApi.js
@@ -0,0 +1,86 @@
+export const MoviesApi = [
+  {
+    _id: "5fedeef47c0ffe00176db1ca",
+    title: "Adam à travers le temps",
+    rate: 4,
+    date: "28 février 2022",
+    imgUrl:
+    "https://i.pinimg.com/originals/4d/ea/be/4deabeb03554cacdba17995886789005.jpg",
+    description:
+    "Après un atterrissage forcé en 2022, le pilote de chasse et voyageur dans le temps Adam Reed fait équipe avec une version de lui-même âgée de 12 ans pour sauver le futur.",
+    category: "Drame",
+    __v: 0,
+  },
+  {
+    _id: "5fedefe47c0ffe00176db1cb",
+    title: "El Jaida",
+    rate: 5,
+    date: "2017",
+    imgUrl:
+    "https://pictures.artify.tn/media/cwnbdwr0zttavvcnwopv.jpg?width=200",
+    description:
+    "Quatre femmes se retrouvent à Dar Joued (prison de femmes) huit mois avant l'indépendance de la Tunisie (entre octobre 1954 et juin 1955). D'âges et de conditions sociales différents, elles sont condamnées à cohabiter en subissant l'autorité et les injustices de leur geôlière : la jaida. Elles vont partager souvenirs du monde extérieur, joie, émotions et détresse de leur quotidien",
+    category: "Drame",
+    __v: 0,
+  },
+  {
+    _id: "5fedf02d7c0ffe00176db1cc",
+    title:  "Derrière Les Portes Fermées",
+    rate: 4,
+    date: "2014",
+    imgUrl:
+    "https://pictures.artify.tn/media/xw8q1t1ff8axgjlc0clp.png?width=200",
+    description:
+    "Une jeune femme victime de harcèlement sexuel de la part du directeur de la nouvelle entreprise.",
+    category: "Fiction",
+    __v: 0,
+  },
+  {
+    _id: "5fedf0647c0ffe00176db1cd",
+    title: "Papillon d'or ",
+    rate: 5,
+    date: "2021",
+    imgUrl:
+      "https://pictures.artify.tn/media/ftoqq8jmkfpwb3ym87mo.png?width=200",
+    description:
+    "Moez, policier trentenaire au passé sanglant, rencontre un petit garçon avec qui il entreprend un voyage fantastique….",
+    category: "Fiction",
+    __v: 0,
+  },
+    {
+      _id: "5fedf0947c0ffe00176db1ce",
+      title: "Little Miss Sunshine",
+      rate: 3,
+      date: "2006",
+      imgUrl:
+        "https://mir-s3-cdn-cf.behance.net/project_modules/disp/b1330b14202071.5627f4dc23472.jpg",
+      description:
+        "A family determined to get their young daughter into the finals of a beauty pageant take a cross-country trip in their VW bus.",
+      category: "Comedy | Drama",
+      __v: 0,
+    },
+    {
+      _id: "5fedf0cf7c0ffe00176db1cf",
+      title: "KILL BILL: VOL. 1",
+      rate: 5,
+      date: "2003",
+      imgUrl:
+        "https://i.pinimg.com/564x/32/49/1c/32491ccd2f4ce577dea8871988c5828c.jpg",
+      description:
+        "The lead character, called 'The Bride,' was a member of the Deadly Viper Assassination Squad, led by her lover 'Bill.'The Bride' decided to escape her life as a killer.",
+      category: "Action",
+      __v: 0,
+    },
+    {
+      _id: "5fedf1067c0ffe00176db1d0",
+      title: "The Wolf Of Wall Street",
+      rate: 5,
+      date: "2013",
+      imgUrl:
+        "https://i.pinimg.com/originals/3e/f1/ba/3ef1baaaceb5a95c4f57a7cb2393b39d.jpg",
+      description:
+        "Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.",
+      category: "Biography | Crime | Drama",
+      __v: 0,
+    },
+  ];
